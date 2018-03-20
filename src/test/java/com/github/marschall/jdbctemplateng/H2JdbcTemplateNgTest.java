b0ba2c0ce222b454ddfcfa3dcbca21e571bcd03d
Philippe Marschall <philippe.marschall@gmail.com>
2018-03-20 06:48:32 +0100

    switch to latest h2

--- src/test/java/com/github/marschall/jdbctemplateng/H2JdbcTemplateNgTest.java
+++ src/test/java/com/github/marschall/jdbctemplateng/H2JdbcTemplateNgTest.java
@@ -1,20 +1,14 @@
 package com.github.marschall.jdbctemplateng;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Arrays;
-import java.util.List;
 
 import javax.sql.DataSource;
 
 import org.h2.jdbcx.JdbcDataSource;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
 
 class H2JdbcTemplateNgTest extends AbstractJdbcTemplateNgTest {
 
@@ -56,25 +50,6 @@ class H2JdbcTemplateNgTest extends AbstractJdbcTemplateNgTest {
   @Override
   boolean largeUpdateSupported() {
     return false;
-  }
-
-
-
-  @Test
-  void testBatchUpdateGeneratedKey() {
-    // work around for https://github.com/h2database/h2database/pull/955
-    SimpleDTO dto1 = new SimpleDTO(23);
-    SimpleDTO dto2 = new SimpleDTO(42);
-
-    List<SimpleDTO> dtos = Arrays.asList(dto1, dto2);
-    List<FailedUpdate<SimpleDTO>> failedUpdates = this.getJdbcTemplate()
-            .batchUpdate("INSERT INTO test_table(test_value) VALUES (?)", Statement.RETURN_GENERATED_KEYS)
-            .binding(dtos, 10, (preparedStatement, dto) -> preparedStatement.setInt(1, dto.getTestValue()))
-            .forFailedUpdates(resultSet -> resultSet.getInt(1), SimpleDTO::setPrimaryKey);
-
-    assertThat(failedUpdates).isEmpty();;
-    assertNotNull(dto1.getPrimaryKey());
-    assertNotNull(dto2.getPrimaryKey());
   }
 
 }
